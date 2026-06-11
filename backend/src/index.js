804c71e4bde277ad7949c1efe37e76b0a919ce99
Pranav-Sharma-Code <pranavofficial2006@gmail.com>
2026-06-11 20:26:49 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,11 @@ const app = express();
 connectDB();
 
 //     middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+}));
+
 app.use(express.json());
 
 //    ROutes
